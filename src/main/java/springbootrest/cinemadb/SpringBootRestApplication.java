96d1be3e4fbefd296304c4dee5bd71c854c52798
george <gaevstafiev@yahoo.com>
2021-07-01 12:36:04 +0300

    рабочая версия без сваггера

--- src/main/java/springbootrest/cinemadb/SpringBootRestApplication.java
+++ src/main/java/springbootrest/cinemadb/SpringBootRestApplication.java
@@ -3,10 +3,7 @@ package springbootrest.cinemadb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"springbootrest.cinemadb",
-                                            "springbootrest.cinemadb.service",
-                                            "springbootrest.cinemadb.model",
-                                            "springbootrest.cinemadb.controllers"})
+@SpringBootApplication
 public class SpringBootRestApplication {
 
     public static void main(String[] args) {
